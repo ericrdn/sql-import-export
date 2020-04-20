0a50c2997ee549097258d8f485d0eed532d0d9e5
Eric <eric.rdn@gmail.com>
2020-04-19 23:51:14 -0300

    Inicio da Implementação

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using Dapper;
+using Newtonsoft.Json;
+
+namespace sql_import
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conectando...");
+
+            var conn = new SqlConnection(@"Server=localhost;Database=DBTESTE;User Id=sa;Password=<YourStrong!Passw0rd>;");
+
+            Console.WriteLine("Efetuando Consulta...");
+
+            var Dados = conn.Query("SELECT * FROM DADOS");
+
+            Console.WriteLine("Convertendo para JSON...");
+
+            using (StreamWriter file = File.CreateText(@"dados.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, Dados);
+            }
+
+        }
+    }
+}
