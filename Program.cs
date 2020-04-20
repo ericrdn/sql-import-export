6984f6a589d69eddaa73cce6415c36dc9bc7b144
Eric <eric.rdn@gmail.com>
2020-04-20 00:11:02 -0300

    Ajuste

--- Program.cs
+++ Program.cs
@@ -8,23 +8,56 @@ namespace sql_import
 {
     class Program
     {
+
+        static void Log(string Msg)
+        {
+            Console.WriteLine($"{DateTime.Now:dd/MM/yyyy HH:mm:ss.fff} - {Msg}");
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Conectando...");
 
-            var conn = new SqlConnection(@"Server=localhost;Database=DBTESTE;User Id=sa;Password=<YourStrong!Passw0rd>;");
+            var conn = new SqlConnection($@"Server=localhost;Database=master;User Id=sa;Password=<YourStrong!Passw0rd>;");
 
-            Console.WriteLine("Efetuando Consulta...");
+            Log("Levantando bases de dados...");
 
-            var Dados = conn.Query("SELECT * FROM DADOS");
+            var BancosdeDados = conn.Query("SELECT * FROM SYS.sysdatabases WHERE DBID > 4");
+
+            foreach (var item in BancosdeDados)
+            {
+                Log($"{item.name}...");
+
+                using (var conn_db = new SqlConnection($@"Server=localhost;Database={item.name};User Id=sa;Password=<YourStrong!Passw0rd>;"))
+                {
+                    var Tabelas = conn_db.Query("SELECT * FROM SYSOBJECTS WHERE XTYPE = 'U' ");
+
+                    foreach (var tabela in Tabelas)
+                    {
+                        Log($"{item.name}.{tabela.name} - Consutando...");
+
+                        var Dados = conn_db.Query($"SELECT * FROM {tabela.name} ");
+
+                        Log($"{item.name}.{tabela.name} - Gravando...");
+
+
+
+                        using (StreamWriter file = File.CreateText($@"data\{item.name}.{tabela.name}.json"))
+                        {
+                            JsonSerializer serializer = new JsonSerializer();
+                            serializer.Serialize(file, Dados);
+                        }
+                    }
+                }
+            }
+
+
+
+
 
             Console.WriteLine("Convertendo para JSON...");
 
-            using (StreamWriter file = File.CreateText(@"dados.json"))
-            {
-                JsonSerializer serializer = new JsonSerializer();
-                serializer.Serialize(file, Dados);
-            }
+
 
         }
     }
